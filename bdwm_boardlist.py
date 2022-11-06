91df4877a4e0ebe7d8b8593ced7f973cc2520c00
houkaihua <houkaihua@users.noreply.github.com>
2022-11-06 19:15:31 +0800

    函数入口+简化函数+修复bug
    加了函数入口main函数  简化了函数写法for 修复了boardlist.ans 存放位置错误的问题

--- bdwm_boardlist.py
+++ bdwm_boardlist.py
@@ -5,81 +5,95 @@ from bs4 import BeautifulSoup
 import requests
 from BDWM import BDWM
 import getpass
+import os
+import base64
+import sys
 
 #admin0-9 ABCG https://bbs.pku.edu.cn/v2/board.php?bid={xxx}
-boards_bid=[
-    "621",  #0区目录
-    "664",  #1区目录
-    "671",  #2区目录
-    "673",  #3区目录
-    "674",  #4区目录
-    "675",  #5区目录
-    "678",  #6区目录
-    "679",  #7区目录
-    "680",  #8区目录
-    "681",  #9区目录
-    "682",  #A区目录
-    "683",  #B区目录
-    "685",  #C区目录
-    "686",  #D区目录
-    "687",  #F区目录
-    "688"   #G区目录
-    ]
-
 boardlists_dic = {"BBShelp":"0 0"}
+boards_bid={
+    "0":"621",  #0区目录
+    "1":"664",  #1区目录
+    "2":"671",  #2区目录
+    "3":"673",  #3区目录
+    "4":"674",  #4区目录
+    "5":"675",  #5区目录
+    "6":"678",  #6区目录
+    "7":"679",  #7区目录
+    "8":"680",  #8区目录
+    "9":"681",  #9区目录
+    "A":"682",  #A区目录
+    "B":"683",  #B区目录
+    "C":"685",  #C区目录
+    "D":"686",  #D区目录
+    "F":"687",  #F区目录
+    "G":"688"   #G区目录
+}
 
-userid =input("请输入BBS帐号(推荐使用WMMZ)：")
-password = getpass.getpass("请输入密码(不会显示)：")
-try:
-    bdwm = BDWM('WMWZ', password)
-except BDWM.RequestError as e:
-    # If failing to login, remove wrong password file.
-    print("password error")
-    raise e
+def get_decoded_password(file):
+    """read password and decode it"""
+    if not os.path.exists(file):
+        return None
+    with open(file, 'r') as f:
+        encoded_password = f.read()
+    return base64.b64decode(encoded_password.encode()).decode()
 
-## 使用高权限帐号获取所有版面目录
-for i in range(0,len(boards_bid)):
-    url = "https://bbs.pku.edu.cn/v2/board.php?bid="+boards_bid[i]
-    response = bdwm._session.get(url)
-    soup = BeautifulSoup(response.text, features="lxml")
-    
-    partition = ''
-    if(i < 10):
-        partition = chr(48+i)
-    elif(i < 14):
-        partition = chr(55+i)
-    else:
-        partition = chr(56+i)
+def write_encoded_password(password, file):
+    """encode password and write it"""
+    encoded_password = base64.b64encode(password.encode()).decode()
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    with open(file, 'w') as f:
+        f.write(encoded_password)
 
-    # class = left左边添加收藏夹/ class = upper 列表
-    boardlists_engname = soup.select('.set .upper .eng-name') 
-    for j in range(0,len(boardlists_engname)):
-        boardlists_dic[boardlists_engname[j].text] = "{} 1".format(partition)
-
-    print("正在添加{}区目录".format(partition))
-
-## 使用游客帐号获取所有不登陆可见版面目录,并更新字典
-for i in range(0,len(boards_bid)):
-    url = "https://bbs.pku.edu.cn/v2/board.php?bid="+boards_bid[i]
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, features="lxml")
-    
-    partition = ''
-    if(i < 10):
-        partition = chr(48+i)
-    elif(i < 14):
-        partition = chr(55+i)
-    else:
-        partition = chr(56+i)
-
-    # class = left左边添加收藏夹/ class = upper 列表
-    boardlists_engname = soup.select('.set .upper .eng-name') 
-    for j in range(0,len(boardlists_engname)):
-        boardlists_dic[boardlists_engname[j].text] ="{} 0".format(partition)
+# 根据request权限读取版面目录，并标记mark  0-游客可见 1-仅登录可见
+def get_board_mark(boardlists_dic,request,mark):
+    for board in boards_bid.keys():
+        url = "https://bbs.pku.edu.cn/v2/board.php?bid="+boards_bid[board]
+        #response = bdwm._session.get(url)
+        response = request.get(url)
+        soup = BeautifulSoup(response.text, features="lxml")
         
-    print("{}区已更新完毕".format(partition))
+        # class = left左边添加收藏夹/ class = upper 列表
+        boardlists_engname = soup.select('.set .upper .eng-name') 
+        for j in range(0,len(boardlists_engname)):
+            boardlists_dic[boardlists_engname[j].text] = "{} {}".format(board,mark)
 
-# 格式 版名 分区 是否登录可见（0-游客可见 1-仅登录可见）
-with open('boardlist.ans', 'w+', encoding='utf8') as f:
-    for board in boardlists_dic:
-        f.write(("{} {}\n").format(board,boardlists_dic[board]))
+def main():
+    #pyinstaller 打包时获取正确路径 支持py+exe
+    base_path = ""
+    if getattr(sys, 'frozen', False):  # 判断sys中是否存在frozen变量,
+        base_path = os.path.dirname(sys.executable)
+    else:
+        base_path = os.path.dirname(__file__)
+
+    #支持读取token
+    password_file = os.path.join(base_path, '.token', 'token')
+    password = get_decoded_password(password_file)
+    if not password:
+        password = getpass.getpass("请输入WMWZ的密码(不会显示)：")
+        # Store encoded password in a file, to avoid inputting password every time.
+        write_encoded_password(password, password_file)
+
+    #登录WMWZ并建立session
+    try:
+        bdwm = BDWM('WMWZ',password)
+    except BDWM.RequestError as e:
+        # If failing to login, remove wrong password file.
+        os.remove(password_file)
+        raise e
+
+    print("获取所有版面中...")        
+    get_board_mark(boardlists_dic,bdwm._session,"1")
+    print("已获取所有版面，更新游客可见版面中...")
+    guest = requests
+    get_board_mark(boardlists_dic,guest,"0")
+    print("已更新所有版面，写入文件中...")
+
+    # 写入文件 格式 版名 分区 是否登录可见（0-游客可见 1-仅登录可见）
+    with open(os.path.join(base_path,'boardlist.ans'), 'w+', encoding='utf8') as f:
+        for board in boardlists_dic:
+            f.write(("{} {}\n").format(board,boardlists_dic[board]))
+            print(board)
+
+if __name__ == "__main__":
+    main()
